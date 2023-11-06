aa963fb1777e142f6b2d4a015170af921c3b049a
AOUDAI <aoudair16031999@gmail.com>
2023-11-06 09:14:09 +0100

    add 4-new_in_list.py 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    string = []
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            string.append(my_string[i])
+    return ("".join(string))
