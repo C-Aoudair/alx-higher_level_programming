6ab88b0b05ee9f9728d7807de393268b91807354
AOUDAI <aoudair16031999@gmail.com>
2023-11-08 22:20:58 +0100

    fix tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,10 +4,10 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    ret = list(a_dictionary.keys())[0]
+    key_of_big = list(a_dictionary.keys())[0]
     big = a_dictionary[ret]
-    for c, h in a_dictionary.items():
-        if h > big:
-            big = h
-            ret = c
-    return (ret)
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            key_of_big = k
+    return (key_of_big)
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,5 +4,5 @@
 def uniq_add(my_list=[]):
     result = 0
     for x in set(my_list):
-        result += x
+        result = result + x
     return (result)
