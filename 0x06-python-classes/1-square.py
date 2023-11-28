094b5b723a0326a91d8c9e0c96981a477272c7c1
AOUDAI <aoudair16031999@gmail.com>
2023-11-28 11:39:56 +0100

    add task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a Square class."""
+
+
+class Square:
+    """Square class."""
+
+    def __init__(self, size):
+        """Initialize the class.
+        Args:
+            size (int): The size of the class.
+        """
+
+        self.__size = size
