085cc41f77c6fcaaa4ec9edd52bd06e034043b91
AOUDAI <aoudair16031999@gmail.com>
2023-12-07 12:33:47 +0100

    Fix task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,11 @@
 
 
 def read_file(filename=""):
+    """Reads the file and prints it to stdout.
+    args:
+    filename: The name of the file.
+    """
+
     with open(filename, "r") as file:
         for line in file:
             print(line.rstrip())
