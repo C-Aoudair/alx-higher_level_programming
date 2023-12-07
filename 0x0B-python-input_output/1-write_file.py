9fecf706c77ed31c63e26aad7879fccfd78a126b
AOUDAI <aoudair16031999@gmail.com>
2023-12-07 13:06:14 +0100

    add task1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define wrtie_file function"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file.
+    args:
+        filename: The name of the file.
+        text: The text to be written in the file.
+    Return:
+        The number of characters written.
+    """
+
+    with open(filename, 'w') as file:
+        count = file.write(text)
+    
+    return count
