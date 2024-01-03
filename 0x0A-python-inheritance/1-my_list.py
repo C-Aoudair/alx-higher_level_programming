c057dd2b180bba9a9c807652350b3423159efaf6
AOUDAI <aoudair16031999@gmail.com>
2024-01-03 13:54:23 +0100

    add task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define the MyList class that inherits from list."""
+
+
+class MyList(list):
+    """Inherits from list and contains print_sorted method"""
+
+
+    def print_sorted(self):
+        print(sorted(self))
