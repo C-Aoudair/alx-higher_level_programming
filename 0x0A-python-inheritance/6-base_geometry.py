3890f93c407fc69e2eca1130b65fe3b04d96dea3
AOUDAI <aoudair16031999@gmail.com>
2024-01-07 16:21:11 +0100

    add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+
+    def area(self):
+        """Raises Exception with the massage area() is not implemented."""
+
+        raise Exception("area() is not implementded")
