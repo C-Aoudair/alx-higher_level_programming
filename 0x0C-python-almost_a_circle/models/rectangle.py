ac7359ddd61cd8309de7d50916c5706ab96ae6eb
AOUDAI <aoudair16031999@gmail.com>
2024-01-08 23:58:37 +0100

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines Rectangle class."""
-from models.base import Base
+import models.base
 
 
 class Rectangle(Base):
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """Test Rectangle class."""
-
-
+import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 
+class TestRectangle(unittest.TestCase):
+    def test_0(self):
+        r1 = Rectangle(10, 2)
+        self.assertTrue(r1.id == 1)
 
+        r2 = Rectangle(8, 8, 3, 8, 10)
+        self.assertTrue(r2.id == 10)
