9559c138f3dfc225577d189470f392b1779141e9
AOUDAI <aoudair16031999@gmail.com>
2024-02-16 09:08:52 +0100

    test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,21 +5,13 @@ import sys
 
 args = sys.argv
 
-try:
-    db_connection = MySQLdb.connect("localhost", args[1], args[2], args[3])
-
-except Exception as error:
-    sys.exit(1)
-
+db_connection = MySQLdb.connect("localhost", args[1], args[2], args[3])
 cursor = db_connection.cursor()
 table = f"{sys.argv[3]}.states"
 
-try:
-    cursor.execute(f"SELECT * FROM {table} WHERE name LIKE 'N%';")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(f"{row}")
+cursor.execute(f"SELECT * FROM {table} WHERE name LIKE 'N%';")
+rows = cursor.fetchall()
+for row in rows:
+    print(f"{row}")
 
-except Exception as error:
-    db_connection.close()
-    sys.exit(1)
+db_connection.close()
