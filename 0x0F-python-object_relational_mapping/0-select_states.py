d564596fc93cea9271df3d5e02f26e544b73a0ea
AOUDAI <aoudair16031999@gmail.com>
2024-02-16 08:47:45 +0100

    change task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#this module for exicuting sql command via MySQLdb modeule.
+"""this module for exicuting sql command via MySQLdb modeule."""
 import MySQLdb
 import sys
 
@@ -7,13 +7,13 @@ args = sys.argv
 try:
     db_connection = MySQLdb.connect("localhost", args[1], args[2], args[3])
 
-except:
+except Exception as error:
     print("Can't connect to database")
     sys.exit(1)
 
 cursor = db_connection.cursor()
 table = f"{sys.argv[3]}.states"
-cursor.execute(f"SELECT * FROM {table} WHERE name LIKE 'N%';")
+cursor.execute(f"SELECT * FROM hbtn_0e_0_usa.states WHERE name LIKE 'N%';")
 rows = cursor.fetchall()
 for row in rows:
     print(f"{row}")
