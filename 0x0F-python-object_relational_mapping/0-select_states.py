eb07be79504406eabc3966010a5e79b39c735bc1
AOUDAI <aoudair16031999@gmail.com>
2024-02-16 09:36:20 +0100

    test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""this module for exicuting sql command via MySQLdb modeule."""
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
-    db_connection = MySQLdb.connect(host="localhost",
-                                    port=3306,
-                                    user=sys.argv[1],
-                                    passwd=sys.argv[2],
-                                    database=sys.argv[3])
-    cursor = db_connection.cursor()
-
-    cursor.execute(f"SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;")
-    rows = cursor.fetchall()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
     for row in rows:
-        print(f"{row}")
-
-    cursor.close()
-    db_connection.close()
+        print(row)
+    cur.close()
+    db.close()
