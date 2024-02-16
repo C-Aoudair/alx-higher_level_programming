ec63b8a126f3066a86df3719d5a46169f8b02168
AOUDAI <aoudair16031999@gmail.com>
2024-02-16 22:58:26 +0100

    test

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  lists all states with name starting with N from database hbtn_0e_0_usa"""
+"""  lists all states with name maechs a user input from db hbtn_0e_0_usa"""
 
 if __name__ == "__main__":
     import MySQLdb
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     db_connect = MySQLdb.connect(host="localhost", user=sys.argv[1],
                                  passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db_connect.cursor()
-    cursor.execute(f"""SELECT * FROM states WHERE name
-                   LIKE BINARY 'arizona' ORDER BY id;""")
+    cursor.execute(f"""SELECT * FROM states
+                   WHERE name = '{sys.argv[4]}' ORDER BY id;""")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
