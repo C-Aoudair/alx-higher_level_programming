c028646de4a80984e696ac826d045af763a587b4
AOUDAI <aoudair16031999@gmail.com>
2024-02-16 09:34:08 +0100

    test changes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,17 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    try:
-        db_connection = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
-        cursor = db_connection.cursor()
+    db_connection = MySQLdb.connect(host="localhost",
+                                    port=3306,
+                                    user=sys.argv[1],
+                                    passwd=sys.argv[2],
+                                    database=sys.argv[3])
+    cursor = db_connection.cursor()
 
-        cursor.execute(f"SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(f"{row}")
+    cursor.execute(f"SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f"{row}")
 
-        db_connection.close()
-
-    except Exception as error:
-        pass
+    cursor.close()
+    db_connection.close()
