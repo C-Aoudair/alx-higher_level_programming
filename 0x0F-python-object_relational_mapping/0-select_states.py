82380be742bbab61099871db63c14f8437d24971
AOUDAI <aoudair16031999@gmail.com>
2024-02-16 08:50:21 +0100

    change task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,16 +4,16 @@ import MySQLdb
 import sys
 
 args = sys.argv
-try:
-    db_connection = MySQLdb.connect("localhost", args[1], args[2], args[3])
+"""try:"""
+db_connection = MySQLdb.connect("localhost", args[1], args[2], args[3])
 
-except Exception as error:
+"""except Exception as error:
     print("Can't connect to database")
-    sys.exit(1)
+    sys.exit(1):"""
 
 cursor = db_connection.cursor()
 table = f"{sys.argv[3]}.states"
-cursor.execute(f"SELECT * FROM hbtn_0e_0_usa.states WHERE name LIKE 'N%';")
+cursor.execute(f"SELECT * FROM {table} WHERE name LIKE 'N%';")
 rows = cursor.fetchall()
 for row in rows:
     print(f"{row}")
