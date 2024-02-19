3cf087259aaa9e175932ab5312d8776c1136d8b0
AOUDAI <aoudair16031999@gmail.com>
2024-02-19 10:17:25 +0100

    clean

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,15 +3,13 @@
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
-from sqlalchemy.orm import relationship
 
 
 class City(Base):
     """Represent a cities table in MySQL database"""
 
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
 
     id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    """state = relationship("State", back_populates="city")"""
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,6 @@
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -11,8 +10,7 @@ Base = declarative_base()
 class State(Base):
     """Represent a States table in MySQL database"""
 
-    __tablename__ = 'states'
+    __tablename__ = "states"
 
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
-    """city = relationship("City", back_populates='state')"""
