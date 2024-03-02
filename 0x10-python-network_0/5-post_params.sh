13d375605d3411e1b2faea33a7ccf70fbc7066f6
AOUDAI <aoudair16031999@gmail.com>
2024-03-02 14:00:22 +0100

    fix task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to a URL with variables
-curl -sX POST -d "email=5-post_params.sh&subject=I will always be here for PLD" $1
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" $1
