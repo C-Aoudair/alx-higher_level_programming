f65929b0426937e3901781ba9da5deec08a607e2
AOUDAI <aoudair16031999@gmail.com>
2024-03-04 00:59:07 +0100

    test

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-curl -sL -X PUT 0.0.0.0:5000/catch_me -d "name=no-name" -H "Origin: alx-se"
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: alx-se"
