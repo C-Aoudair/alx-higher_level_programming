dba1b951e1c6c09b824ee5d54940d5a7c375828c
AOUDAI <aoudair16031999@gmail.com>
2024-03-25 18:22:57 +0100

    add task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Script that fetches https://alx-intranet.hbtn.io/status."""
+
+from urllib import request
+
+with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    data = response.read()
+    print(f"\t- type: {type(data)}")
+    print(f"\t- content: {data}")
+    print(f"\t- utf8 content: {data.decode('utf-8')}")
