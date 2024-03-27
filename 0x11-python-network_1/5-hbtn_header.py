72e7ea6a2e2e4df525dfae1526760a4ab31f3e58
AOUDAI <aoudair16031999@gmail.com>
2024-03-27 18:24:38 +0100

    add task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Displays the value of the X-Request-Id in response header"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
