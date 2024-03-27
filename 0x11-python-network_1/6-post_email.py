882e0e4b9ef2bcaf706823df014923f7404c4d2f
AOUDAI <aoudair16031999@gmail.com>
2024-03-27 18:36:10 +0100

    test

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ sends a POST request to the passed URL with the email
-as a parameter, and displays the body of"""
+as a parameter, and displays the body of the response"""
 
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    r = request.post(sys.argv[1], data={'email': sys.argv[2])
+    r = request.post(sys.argv[1], data={'email': sys.argv[2]})
     print(r.text)
