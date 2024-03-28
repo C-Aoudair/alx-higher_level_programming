2599780c8b131959e8a4da1a1da485eda5656671
AOUDAI <aoudair16031999@gmail.com>
2024-03-28 15:59:32 +0100

    test task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
+    r = requests.get(url, params={'per_page': 10})
+
+    if r.status_code == 200:
+        commits = r.json()
+        for commit in commits:
+            commitMessage = commit['commit']['message']
+            authorName = commit['commit']['author']['name']
+            print(f"{commitMessage}: {authorName}")
