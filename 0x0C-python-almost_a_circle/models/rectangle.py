53e55e0e8e45e81d9feb4bd1f35617747b7dc9bc
C-Aoudair <aoudair16031999@gmail.com>
2024-06-30 13:40:22 +0100

    add area and display methods to Rectangle calss

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -60,3 +60,10 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
+    
+    def area(self):
+        return self.width * self.height
+    
+    def display(self):
+        for i in range(self.height):
+            print('#' * self.width)
